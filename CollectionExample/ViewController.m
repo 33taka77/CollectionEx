aff303f195a3283f03e41eac71940e0557221618
相澤 隆志 <aizawakeisuke@aisawa-no-MacBook-Air.local>
2014-02-23 22:56:00 +0900

    初期画面まで

--- CollectionExample/ViewController.m
+++ CollectionExample/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (nonatomic, weak) IBOutlet UIToolbar* toolBar;
+@property (nonatomic, weak) IBOutlet UITextField* textField;
+@property (nonatomic, weak) IBOutlet UIBarButtonItem* shareButton;
+- (IBAction)shareButtonTapped:(id)sender;
 
 @end
 
@@ -18,12 +22,26 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_cork.png"]];
+    UIImage* navBarImage = [[UIImage imageNamed:@"navbar.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(17, 27, 27, 27)];
+    [self.toolBar setBackgroundImage:navBarImage forToolbarPosition:UIToolbarPositionAny barMetrics:UIBarMetricsDefault];
+    
+    UIImage* shareButtonImage = [[UIImage imageNamed:@"button.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(8, 8, 8, 8)];
+    [self.shareButton setBackButtonBackgroundImage:shareButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    
+    UIImage* textFieldImage = [[UIImage imageNamed:@"search_field.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 10, 10, 10)];
+    [self.textField setBackground:textFieldImage];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)shareButtonTapped:(id)sender
+{
+    
 }
 
 @end
