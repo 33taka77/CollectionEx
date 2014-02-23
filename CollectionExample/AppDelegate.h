bffc0962029ad5c58734330b8df582d4aa94f2a5
相澤 隆志 <aizawakeisuke@aisawa-no-MacBook-Air.local>
2014-02-23 22:11:19 +0900

    Initial Commit

---
+++ CollectionExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionExample
+//
+//  Created by 相澤 隆志 on 2014/02/23.
+//  Copyright (c) 2014年 相澤 隆志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CollectionExample
+//
+//  Created by 相澤 隆志 on 2014/02/23.
+//  Copyright (c) 2014年 相澤 隆志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
